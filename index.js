a700f4ad8d98f87a855d688779be3c2f07d62411
cjc <cjc@cjc.id.au>
2011-01-09 15:28:03 +1000

    Change to generate shell script instead of executing id3 with child process calls

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ var lvl0 = /\/?([^-]*)()()-\s*(.*)\.mp3$/;
 
 var regex = [lvl0,lvl1,lvl2];
 
+var eecs = [];
+
 var baseDir = process.argv[2];
-console.log("Base Dir  - " + baseDir);
+//console.log("Base Dir  - " + baseDir);
 
 var cmdlineesc = function(str) {
 return str.replace(/'/g,"\'\\\'\'");
@@ -28,29 +30,32 @@ var HandleDir = function(dir, level) {
     var bit = fullpath.substring(baseDir.length, fullpath.length);
 
     if (stat.isDirectory()) {
-      console.log("DIR -> " + level + " "  + bit);
+//      console.log("DIR -> " + level + " "  + bit);
       HandleDir(fullpath,level+1);      
     } else if (stat.isFile()) {
       var matches = bit.match(regex[level]);
-      //console.log("FIL -> " + level + " " + bit);
-      //console.dir(matches);
-      //console.log("id3 -l '" + cmdlineesc(fullpath) + "'");
-      var cmdline;
-      if (matches[3] == '') {
-        cmdline = "id3 -a '" + cmdlineesc(matches[1]) + "' -A '" + cmdlineesc(matches[2]) + "' -t '" + cmdlineesc(matches[4]) + "'  '" + cmdlineesc(fullpath) + "'";
-      } else {
-        cmdline = "id3 -a '" + cmdlineesc(matches[1]) + "' -A '" + cmdlineesc(matches[2]) + "' -T '" + cmdlineesc(matches[3]) + "' -t '" + cmdlineesc(matches[4]) + "'  '" + cmdlineesc(fullpath) + "'";
+      if (matches != null) {
+        //console.log("FIL -> " + level + " " + bit);
+        //console.dir(matches);
+        //console.log("id3 -l '" + cmdlineesc(fullpath) + "'");
+        var cmdline;
+        if (matches[3] == '') {
+          cmdline = "id3 -a '" + cmdlineesc(matches[1]) + "' -A '" + cmdlineesc(matches[2]) + "' -t '" + cmdlineesc(matches[4]) + "'  '" + cmdlineesc(fullpath) + "'";
+        } else {
+          cmdline = "id3 -a '" + cmdlineesc(matches[1]) + "' -A '" + cmdlineesc(matches[2]) + "' -T '" + cmdlineesc(matches[3]) + "' -t '" + cmdlineesc(matches[4]) + "'  '" + cmdlineesc(fullpath) + "'";
+        }
+        console.log(cmdline);
+        eecs[eecs.length] = cmdline;
+        //exec("id3 -l '" + cmdlineesc(fullpath) + "'",
+        //exec(cmdline,
+        //  function (error, stdout, stderr) {
+        //    //sys.print('stdout: ' + stdout);
+        //    //sys.print('stderr: ' + stderr);
+        //    if (error !== null) {
+        //      console.log('exec error: ' + error);
+        //    }
+        //});
       }
-      console.log(cmdline);
-      //exec("id3 -l '" + cmdlineesc(fullpath) + "'",
-      exec(cmdline,
-        function (error, stdout, stderr) {
-          //sys.print('stdout: ' + stdout);
-          //sys.print('stderr: ' + stderr);
-          if (error !== null) {
-            console.log('exec error: ' + error);
-          }
-      });
     }
   }
 
@@ -64,3 +69,4 @@ if (!baseStats.isDirectory()) {
 }
 
 HandleDir(baseDir, 0);
+
